828ca173982c5c52f0c8196b78ae0d2670bc195f
GrinAnt <kanes@inbox.ru>
2023-03-16 06:11:01 +0300

    исправление замечаний и доработка меню

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // ПЕРЕМЕННЫЕ
 let hamburger = document.querySelector(".hamburger");
 let navigation = document.querySelector(".navigation");
+let link = document.querySelectorAll(".navigation__link");
 
 // МЕНЮ ГАМБУРГЕР И ПОЯВЛЕНИЕ МЕНЮ
 hamburger.addEventListener("click", function (event) {
@@ -8,6 +9,21 @@ hamburger.addEventListener("click", function (event) {
   navigation.classList.toggle("navigation--active");
 });
 
+// закрытие меню при нажатии на один из пунктов меню
+link.forEach(function (e) {
+  e.addEventListener("click", function () {
+    if (hamburger.classList.contains("hamburger--active")) {
+      hamburger.classList.remove("hamburger--active");
+      navigation.classList.remove("navigation--active");
+    }
+  });
+});
+
+//
+//
+//
+//
+//
 // let arrow = document.querySelectorAll(".slider__arrow");
 // let slide = document.querySelector(".slider__list");
 // let firstImg = slide.querySelectorAll(".slider__item")[0];
@@ -22,3 +38,7 @@ hamburger.addEventListener("click", function (event) {
 //     }
 //   });
 // });
+//
+//
+//
+//
