913d5085e72a1b41fd1a339393305cd872f70709
GrinAnt <kanes@inbox.ru>
2023-03-19 04:12:03 +0300

    Сделал слайдер отзывов

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ link.forEach(function (e) {
   });
 });
 
-// слайдер на slick
+// СЛАЙДЕР НА SLICK
 $(".single-item").slick({
   infinite: true,
   speed: 600,
@@ -27,4 +27,57 @@ $(".single-item").slick({
   swipe: true,
   prevArrow: '<button type="button" class="slider__arrow slider__arrow--left"></button>',
   nextArrow: '<button type="button" class="slider__arrow slider__arrow--right"></button>',
+});
+
+// СЛАЙДЕР ОТЗЫВОВ
+const slider = document.querySelector(".reviews-card");
+const sliderItems = document.querySelectorAll(".reviews-card__item");
+const sliderList = document.querySelector(".reviews-card__list");
+const slideControls = document.querySelectorAll(".reviews-control__item");
+
+let sliderCount = 0;
+let sliderWidth = slider.offsetWidth;
+
+$(".reviews-control__link").on("click", function (e) {
+  e.preventDefault();
+});
+
+// Переключение слайдера отзывов, если бы были кнопки
+// const nextSlide = document.querySelector(".reviews-card__next");
+// const prevSlide = document.querySelector(".reviews-card__prev");
+
+// nextSlide.addEventListener("click", nextSlider);
+// prevSlide.addEventListener("click", prevSlider);
+
+// function nextSlider() {
+//   sliderCount++;
+//   if (sliderCount >= sliderItems.length) {
+//     sliderCount = 0;
+//   }
+//   rollSlider();
+// }
+
+// function prevSlider() {
+//   sliderCount--;
+//   if (sliderCount < 0) {
+//     sliderCount = sliderItems.length - 1;
+//   }
+//   rollSlider();
+// }
+
+function rollSlider() {
+  sliderList.style.transform = `translateX(${-sliderCount * sliderWidth}px)`;
+}
+
+function thisSlide(index) {
+  slideControls.forEach((item) => item.classList.remove("reviews-control__item--active"));
+  slideControls[index].classList.add("reviews-control__item--active");
+}
+
+slideControls.forEach((dot, index) => {
+  dot.addEventListener("click", () => {
+    sliderCount = index;
+    rollSlider();
+    thisSlide(sliderCount);
+  });
 });
